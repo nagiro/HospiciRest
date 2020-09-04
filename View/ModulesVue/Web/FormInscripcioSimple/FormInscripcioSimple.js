e1904f93bb65c238dfba80320d2332a287b573ff
Albert Johé <albert.johe@gmail.com>
2020-09-04 13:59:22 +0200

    Carrego botó loading

--- View/ModulesVue/Web/FormInscripcioSimple/FormInscripcioSimple.js
+++ View/ModulesVue/Web/FormInscripcioSimple/FormInscripcioSimple.js
@@ -45,7 +45,8 @@ Vue.component('form-inscripcio-simple', {
                     ErrorInscripcio: '',
                     ConfirmoAssistencia: false,
                     TPV: {},
-                    isAdmin: (this.Token[0] == this.DetallCurs.CURSOS_SiteId)
+                    isAdmin: (this.Token[0] == this.DetallCurs.CURSOS_SiteId),
+                    Loading: false
                 }
     },    
     computed: {
@@ -159,7 +160,9 @@ Vue.component('form-inscripcio-simple', {
             
             if( ValidaDNI(this.DNI) ) {
                 this.classDNI = 'form-control is-valid';
+                this.Loading = true;
                 axios.get( CONST_api_web + '/ExisteixDNI', {'params': {'DNI': this.DNI, 'idCurs': this.DetallCurs.CURSOS_IdCurs, 'IsRestringit': this.DetallCurs.CURSOS_IsRestringit }}).then( X => {                    
+                    this.Loading = false;
                     if( X.data.PotMatricularCursRestringit.IsOk || this.isAdmin ) {
                         if(X.data.ExisteixDNI) {
                             this.Pas = 2;
@@ -185,7 +188,7 @@ Vue.component('form-inscripcio-simple', {
                 this.classDNI = 'form-control is-invalid';                
             }
         },
-        keymonitor: function($event) {                        
+        keymonitor: function() {                        
             const ValNom = (this.Nom.length > 1);
             const ValCog1 = (this.Cog1.length > 1);
             const ValTelefon = ValidaTelefon( this.Telefon );
@@ -205,10 +208,14 @@ Vue.component('form-inscripcio-simple', {
             this.classAnyNaixement = (!ValAnyNaixement) ? 'form-control is-invalid' : 'form-control is-valid';
             
             if( ValNom && ValCog1 && ValTelefon && ValEmail && ValAnyNaixement ) {
-                this.Pas = 4;
+                return true;
             } else {
-                this.Pas = 1;
+                return false;
             }
+        },
+        botoDadesUsuari: function() {
+            if(this.keymonitor()) this.Pas = 4;
+            else this.Pas = 1;
         },
         setLocalitat: function(fila, seient) {
                                     
@@ -287,7 +294,9 @@ Vue.component('form-inscripcio-simple', {
             $FD.append('Localitats', JSON.stringify(this.Localitats));
             $FD.append('Token', JSON.stringify(this.Token));
             
+            this.Loading = true;
             axios.post( CONST_api_web + '/AltaUsuariSimple', $FD ).then( X => {
+                this.Loading = false;
                 if(X.data.AltaUsuari && X.data.AltaUsuari.MATRICULES.length > 0) {
                     // Si el pagament és amb targeta, anem a la nova web per a fer el pagament
                     if(X.data.AltaUsuari.TPV) {
@@ -332,11 +341,14 @@ Vue.component('form-inscripcio-simple', {
             </div>
 
             <div class="row" v-if="Pas == 0">
-                <div class="col">
+                <div v-show="!Loading" class="col">
                     <label for="DNI">DNI/NIE</label>
-                    <input type="text" :class="classDNI" id="DNI" v-on:keyup="dnikeymonitor" v-model="DNI" aria-describedby="DNI" placeholder="Escriu el DNI/NIE..." /> 
+                    <input type="text" :class="classDNI" id="DNI" v-on:keyup="dnikeymonitor" v-model="DNI" aria-describedby="DNI" placeholder="Escriu el DNI/NIE..." />                         
                     <small id="DNIHelp" class="form-text text-muted">Entri el seu DNI per apuntar-se. </small>                
                 </div>            
+                <div class="col" v-show="Loading">
+                    <div class="alert alert-info">Carregant...</div>
+                </div>
             </div>
             
             <div v-if="Pas == 2 && !LlistaEsperaActiu" class="row alert alert-success"> Hem trobat el seu DNI a la nostra base de dades. <br />Pot seguir amb la inscripció! </div>
@@ -344,56 +356,63 @@ Vue.component('form-inscripcio-simple', {
              
             <div v-if="Pas == 1" class="row alert alert-warning"> No hem trobat el seu DNI a la nostra base de dades. <br />Si és tant amable, ens hauria d'informar del seu nom, telèfon i email per si hem de posar-nos en conacte amb vostè per a poder seguir amb la inscripció. </div>
             
-            <div class="row" v-if="Pas == 1 || Pas == 4">                                                
-                <div class="col">
-                    <label for="NomComplet">Nom</label>
-                    <input type="text" :class="classNom" v-on:blur="keymonitor" v-model="Nom" id="NomComplet" aria-describedby="Nom complet" placeholder="Escriu el nom..." /> 
-                    <small id="NomCompletHelp" class="form-text text-muted">Entri el seu nom complet.</small>                
+            <div v-if="Pas == 1">
+                <div class="row">                                                
+                    <div class="col">
+                        <label for="NomComplet">Nom</label>
+                        <input type="text" :class="classNom" v-on:blur="keymonitor" v-model="Nom" id="NomComplet" aria-describedby="Nom complet" placeholder="Escriu el nom..." /> 
+                        <small id="NomCompletHelp" class="form-text text-muted">Entri el seu nom complet.</small>                
+                    </div>
+                    <div class="col">
+                        <label for="Cog1">Primer cognom</label>
+                        <input type="text" :class="classCog1" v-on:blur="keymonitor" v-model="Cog1" id="Cog1" aria-describedby="Primer cognom" placeholder="" /> 
+                        <small id="Cog1Help" class="form-text text-muted">Entri el seu primer cognom.</small>                
+                    </div>            
+                    <div class="col">
+                        <label for="Cog2">Segon cognom</label>
+                        <input type="text" :class="classCog2" v-on:blur="keymonitor" v-model="Cog2" id="Cog2" aria-describedby="Segon cognom" placeholder="" /> 
+                        <small id="Cog2Help" class="form-text text-muted">Entri el seu segon cognom, si en té.</small>                
+                    </div>                        
                 </div>
-                <div class="col">
-                    <label for="Cog1">Primer cognom</label>
-                    <input type="text" :class="classCog1" v-on:blur="keymonitor" v-model="Cog1" id="Cog1" aria-describedby="Primer cognom" placeholder="" /> 
-                    <small id="Cog1Help" class="form-text text-muted">Entri el seu primer cognom.</small>                
-                </div>            
-                <div class="col">
-                    <label for="Cog2">Segon cognom</label>
-                    <input type="text" :class="classCog2" v-on:blur="keymonitor" v-model="Cog2" id="Cog2" aria-describedby="Segon cognom" placeholder="" /> 
-                    <small id="Cog2Help" class="form-text text-muted">Entri el seu segon cognom, si en té.</small>                
-                </div>                        
-            </div>
-            <div class="row" v-if="Pas == 1 || Pas == 4">                                                
-                <div class="col">
-                    <label for="telefon">Mòbil</label>
-                    <input type="text" :class="classTelefon" v-on:blur="keymonitor" v-model="Telefon" id="telefon" placeholder="">
-                    <small id="TelefonHelp" class="form-text text-muted">Entri el seu telèfon de contacte.</small>
+                <div class="row">                                                
+                    <div class="col">
+                        <label for="telefon">Mòbil</label>
+                        <input type="text" :class="classTelefon" v-on:blur="keymonitor" v-model="Telefon" id="telefon" placeholder="">
+                        <small id="TelefonHelp" class="form-text text-muted">Entri el seu telèfon de contacte.</small>
+                    </div>
+                    <div class="col">
+                        <label for="Email">Correu electrònic</label>
+                        <input type="text" :class="classEmail" v-on:blur="keymonitor" v-model="Email" id="telefon" placeholder="">
+                        <small id="EmailHelp" class="form-text text-muted">Entri el seu correu electrònic.</small>
+                    </div>            
                 </div>
-                <div class="col">
-                    <label for="Email">Correu electrònic</label>
-                    <input type="text" :class="classEmail" v-on:blur="keymonitor" v-model="Email" id="telefon" placeholder="">
-                    <small id="EmailHelp" class="form-text text-muted">Entri el seu correu electrònic.</small>
-                </div>            
-            </div>
-            <div class="row" v-if="Pas == 1 || Pas == 4">                                                
-                <div class="col">
-                    <label for="municipi">Municipi</label>
-                    <input type="text" :class="classMunicipi" v-on:blur="keymonitor" v-model="Municipi" id="municipi" placeholder="">
-                    <small id="MunicipiHelp" class="form-text text-muted">Opcional: El seu municipi de residència.</small>
-                </div>
-                <div class="col">
-                    <label for="genere">Gènere</label>
-                    <select :class="classGenere" v-on:change="keymonitor" v-model="Genere" id="genere">
-                        <option value="M">Masculí</option>
-                        <option value="F">Femení</option>
-                        <option value="A">Altres</option>
-                    </select>                
-                    <small id="GenereHelp" class="form-text text-muted">Opcional: El seu gènere.</small>
-                </div>            
-                <div class="col">
-                    <label for="anynaixement">Any de naixement</label>
-                    <input type="text" :class="classAnyNaixement" v-on:blur="keymonitor" v-model="AnyNaixement" id="anynaixement" placeholder="">
-                    <small id="AnyNaixementHelp" class="form-text text-muted">Opcional: El seu any de naixement.</small>
-                </div>                        
-            </div>        
+                <div class="row">                                                
+                    <div class="col">
+                        <label for="municipi">Municipi</label>
+                        <input type="text" :class="classMunicipi" v-on:blur="keymonitor" v-model="Municipi" id="municipi" placeholder="">
+                        <small id="MunicipiHelp" class="form-text text-muted">Opcional: El seu municipi de residència.</small>
+                    </div>
+                    <div class="col">
+                        <label for="genere">Gènere</label>
+                        <select :class="classGenere" v-on:change="keymonitor" v-model="Genere" id="genere">
+                            <option value="M">Masculí</option>
+                            <option value="F">Femení</option>
+                            <option value="A">Altres</option>
+                        </select>                
+                        <small id="GenereHelp" class="form-text text-muted">Opcional: El seu gènere.</small>
+                    </div>            
+                    <div class="col">
+                        <label for="anynaixement">Any de naixement</label>
+                        <input type="text" :class="classAnyNaixement" v-on:blur="keymonitor" v-model="AnyNaixement" id="anynaixement" placeholder="">
+                        <small id="AnyNaixementHelp" class="form-text text-muted">Opcional: El seu any de naixement.</small>
+                    </div>                        
+
+                    <div class="col-12">
+                        <button :disabled="!keymonitor()" type="submit" class="btn btn-info" @click.prevent="botoDadesUsuari()">Segueix...</button>
+                    </div>                    
+
+                </div>    
+            </div>    
             
             
             <div v-if="Pas == 4 || Pas == 2">
@@ -446,11 +465,17 @@ Vue.component('form-inscripcio-simple', {
                     <label class="form-check-label" for="Assistire">Confirmo que <b>assistiré a l'acte</b> o que <b>avisaré</b>, a la Casa de Cultura, en cas de no poder-ho fer.</label>
                 </div>
                 
-                <button v-if="LlistaEsperaActiu" :disabled="NoPucSeguir()" type="submit" class="btn btn-danger" @click.prevent="doInscripcio()">Posa'm en espera</button>
-                <small v-if="LlistaEsperaActiu"  id="EmailHelp" class="form-text text-muted">Vostè ha quedat en llista d'espera. Si, en un futur hi ha places disponibles, ens posarem en contacte amb vostè.</small>
-  
-                <button v-if="!LlistaEsperaActiu"  :disabled="NoPucSeguir()" type="submit" class="btn btn-primary" @click.prevent="doInscripcio()">Inscriu-me</button>
-                <small  v-if="!LlistaEsperaActiu" id="EmailHelp" class="form-text text-muted">Nomes podrà prèmer el botó si ha omplert totes les dades necessàries.</small>
+                <div v-show="!Loading">
+                    <button v-if="LlistaEsperaActiu" :disabled="NoPucSeguir()" type="submit" class="btn btn-danger" @click.prevent="doInscripcio()">Posa'm en espera</button>
+                    <small v-if="LlistaEsperaActiu"  id="EmailHelp" class="form-text text-muted">Vostè ha quedat en llista d'espera. Si, en un futur hi ha places disponibles, ens posarem en contacte amb vostè.</small>
+    
+                    <button v-if="!LlistaEsperaActiu"  :disabled="NoPucSeguir()" type="submit" class="btn btn-primary" @click.prevent="doInscripcio()">Inscriu-me</button>
+                    <small  v-if="!LlistaEsperaActiu" id="EmailHelp" class="form-text text-muted">Nomes podrà prèmer el botó si ha omplert totes les dades necessàries.</small>
+                </div>
+
+                <div v-show="Loading" style="margin-top: 2vw">
+                    <div class="alert alert-info">Carregant...</div>
+                </div>
  
             </div>
 
