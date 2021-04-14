a958c3828aab22c3bda0e1488726a9b55b147ef2
Albert Johé <albert.johe@gmail.com>
2021-04-14 12:44:15 +0200

    Afegits Arias pels links superiors

--- View/ModulesVue/Web/barraSuperior/barraSuperior.js
+++ View/ModulesVue/Web/barraSuperior/barraSuperior.js
@@ -30,16 +30,16 @@ Vue.component('barra-superior', {
                 </form>
             </div>
 
-        <nav class="barra_superior">
-            <a target="_new" href="https://twitter.com/casadeculturagi"> <i class="fab fa-twitter"></i> </a>                        
-            <a target="_new" href="https://www.facebook.com/casadeculturadegirona"> <i class="fab fa-facebook-square"></i> </a>
-            <a target="_new" href="https://www.youtube.com/user/casadeculturagi"> <i class="fab fa-youtube"></i> </a>
-            <a target="_new" href="https://www.instagram.com/casadeculturagi"> <i class="fab fa-instagram"></i> </a>                                    
-            <a target="_new" href="mailto:info@casadecultura.cat"> <i class="fas fa-at"></i>&nbsp; info@casadecultura.cat </a>
-            <a target="_new" href="tel:+0034972202013"> <i class="fas fa-phone"></i>&nbsp; 972 20 20 13 </a>                        
-            <a target="_new" href="whatsapp://send?text=Click a Whatsapp de la web&phone=+34691217259&abid=+34691217259"> <i class="fab fa-whatsapp"></i>&nbsp; 691 217 259 </a>       
-            <a target="_new" class="withHand" @click="FormulariButlleti = true"> <i class="fas fa-envelope-open-text"></i>&nbsp; Butlletí </a>                 
-            <a target="_new" href="/pagina/141/reserva-d-espais"> <i class="far fa-file-alt"></i>&nbsp; Reserva espais </a>                        
+        <nav class="barra_superior" role="navigation" aria-label="Enllaços ràpids">
+            <a target="_new" href="https://twitter.com/casadeculturagi" aria-label="Enllaç al Twitter"> <i class="fab fa-twitter"></i> </a>                        
+            <a target="_new" href="https://www.facebook.com/casadeculturadegirona" aria-label="Enllaç al facebook"> <i class="fab fa-facebook-square"></i> </a>
+            <a target="_new" href="https://www.youtube.com/user/casadeculturagi" aria-label="Enllaç al Youtube"> <i class="fab fa-youtube"></i> </a>
+            <a target="_new" href="https://www.instagram.com/casadeculturagi" aria-label="Enllaç a instagram"> <i class="fab fa-instagram"></i> </a>                                    
+            <a target="_new" href="mailto:info@casadecultura.cat" aria-label="Enllaç al Email"> <i class="fas fa-at"></i>&nbsp; info@casadecultura.cat </a>
+            <a target="_new" href="tel:+0034972202013" aria-label="Enllaç al telèfon"> <i class="fas fa-phone"></i>&nbsp; 972 20 20 13 </a>                        
+            <a target="_new" href="whatsapp://send?text=Click a Whatsapp de la web&phone=+34691217259&abid=+34691217259" aria-label="Enllaç al Whatsapp"> <i class="fab fa-whatsapp"></i>&nbsp; 691 217 259 </a>       
+            <a target="_new" class="withHand" @click="FormulariButlleti = true"  aria-label="Enllaç a alta butlletí"> <i class="fas fa-envelope-open-text"></i>&nbsp; Butlletí </a>                 
+            <a target="_new" href="/pagina/141/reserva-d-espais" aria-label="Enllaç a reserva d'espais"> <i class="far fa-file-alt"></i>&nbsp; Reserva espais </a>                        
         </nav>
     </section>
     
