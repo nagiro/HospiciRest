4b8a2b57b06db74cc064dd892c449bb98a6a22b4
Albert Johé <albert.johe@gmail.com>
2021-07-06 14:15:05 +0200

    Auxiliar_UploadFtp funcional

--- Api/web/myapiweb.php
+++ Api/web/myapiweb.php
@@ -341,8 +341,8 @@ class MyAPIWeb extends API
     protected function getUploadFtp() {
         require_once AUXDIR . 'UploadFtp/Auxiliar_UploadFtp.php';        
         if( isset($this->request['idS']) && isset($this->request['node']) ) {
-            $CV = new Auxiliar_UploadFtp();
-            return array($CV->loadArxiusNous( $this->request['idS'], $this->request['node'] ), 200);
+            $CV = new Auxiliar_UploadFtp($this->request['idS']);
+            return array($CV->loadArxiusNous( $this->request['node'] ), 200);
         } else {
             throw new Exception('No hi ha cap site i node amb aquesta codificació.');
         }
--- Auxiliars/UploadFtp/Auxiliar_UploadFtp.php
+++ Auxiliars/UploadFtp/Auxiliar_UploadFtp.php
@@ -5,26 +5,22 @@ require_once DATABASEDIR . 'Tables/OptionsModel.php';
 
 class Auxiliar_UploadFtp {
     
-    public $FTP = array(
-        'ip' => '80.34.222.61',
-        // 'ip' => '192.168.0.8',
-        'port' => '30',
-        'user' => 'casadecultura',
-        'password' => '(ccg@#).',
-        'NodesUrl' => array(1 => array( 1 => '/Actual/WEB/PMP/') )
-    );
+    public $FTP = array();        
 
-    public function __construct($FTP_Data, $idSite) {
-        $OM = new OptionsModel();
+    public function __construct($idSite) {
+        $OM = new OptionsModel();        
         $this->FTP = json_decode($OM->getOption('AUX_UPLOADFTP', $idSite), true);
-        if($this->FTP === FALSE) throw new Exception()
+        if($this->FTP === FALSE) $this->guardoLogError("No he pogut carregar les dades d'upload ftp.");
     }
 
-    public function loadArxiusNous( $idS, $idNode ) {
+    /**
+     * Funció que carrega els arxius nous del servidor nostre, al servidor web per mostrar-lo a la web.
+     */
+    public function loadArxiusNous( $idNode ) {
         
         $conn = $this->ConnectaFTP();        
         $baseUrl = DOCUMENTSDIR . $idNode . '/';
-        $ftpUrl = $this->FTP['NodesUrl'][$idS][$idNode];
+        $ftpUrl = $this->FTP['NODESURL'][$idNode];
         $NewFiles = $this->getNewFiles( $conn , $baseUrl, $ftpUrl );
 
         $this->DownloadFiles( $conn, $NewFiles , $baseUrl, $ftpUrl );
@@ -33,22 +29,44 @@ class Auxiliar_UploadFtp {
         
     }
 
+    /**
+     * Funció que connecta al servidor Ftp mitjançant dades guardades a l'opció AUX_UPLOADFTP
+     */
     function ConnectaFTP() {
-        $conn = ftp_connect($this->FTP['ip'], $this->FTP['port']);
-        ftp_login($conn, $this->FTP['user'], $this->FTP['password']);
-        return $conn;
+        $conn = ftp_connect($this->FTP['IP'], $this->FTP['PORT']);
+        if($conn !== FALSE) {
+            ftp_login($conn, $this->FTP['USER'], $this->FTP['PASSWORD']);
+            return $conn;
+        } else {
+            $this->guardoLogError("No he pogut connectar al ftp. " . print_r($this->FTP, true));
+        }
     }
 
+    /**
+     * Identifico els arxius nous i els carregaré. 
+     */    
     function getNewFiles( $conn , $baseUrl, $ftpUrl ) {        
-        $files = ftp_nlist($conn, $ftpUrl);
+        $ArxiusAlWeb = array_diff(scandir($baseUrl), array('..', '.'));        
+        $ArxiusAlFtp = ftp_nlist($conn, $ftpUrl);     // Agafo els arxius de l'ftp ( el servidor local )
         $NewFiles = array();
-        foreach( $files as $file ) {
-            $f = basename($file);
-            $timeFtp = ftp_mdtm($conn, $ftpUrl . $f);
+        foreach( $ArxiusAlFtp as $FileAtFTP ) {            //Per cada arxiu que no sigui al web o que sigui més nou al nostre servidor, el carrego. 
+            $file_at_ftp = basename($FileAtFTP);            
+            $timeFtp = ftp_mdtm($conn, $ftpUrl . $file_at_ftp);
             $timeLocal = 0;
-            if( file_exists( $baseUrl . $f ) ) $timeLocal = filemtime( $baseUrl . $f );            
-            if($timeLocal < $timeFtp ) $NewFiles[] = $f;
+            if( !is_dir($baseUrl)) mkdir($baseUrl);
+            if( file_exists( $baseUrl . $file_at_ftp ) ) $timeLocal = filemtime( $baseUrl . $file_at_ftp );            
+            if($timeLocal < $timeFtp ) $NewFiles[] = $file_at_ftp;
         }
+
+        //Per tots els arxius del web que no siguin al ftp, els esborro per no mostrar-los.                
+        foreach($ArxiusAlWeb as $ArxiuWeb) {
+            $trobat = false;
+            foreach($ArxiusAlFtp as $ArxiuFtp) {
+                if($ArxiuWeb == basename($ArxiuFtp)) $trobat = true;
+                continue;
+            }
+            if(!$trobat) unlink( $baseUrl . $ArxiuWeb );
+        }            
 
         return $NewFiles;
     }    
@@ -56,11 +74,19 @@ class Auxiliar_UploadFtp {
     function DownloadFiles( $conn, $NewFiles, $baseUrl, $ftpUrl ) {
         foreach( $NewFiles as $file ) {
             $result = ftp_get($conn, $baseUrl . $file, $ftpUrl . $file, FTP_BINARY);
-            if($result === FALSE) $this->guardoLogError();
+            if($result === FALSE) $this->guardoLogError("No he pogut carregar l'arxiu: {$file}");
         }
     }
 
-    function guardoLogError() {
+    function guardoLogError($Missatge) {
+        $Data = date("d/M/Y H:i:s");
+        $TextToSave = $Data . ' | '.$Missatge;
+
+        $FileName = date('Y-m') . '-log.txt';
+
+        file_put_contents( LOGSDIR . $FileName , $TextToSave, FILE_APPEND );
+
+        die;
         
     }
 
