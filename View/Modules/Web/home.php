c173c608be34cb3d24951a4f3ca1db0de741ab2d
Albert Johe <ajohe@ddgi.cat>
2023-02-21 14:21:07 +0100

    Eliminat logo 200 anys

--- View/Modules/Web/home.php
+++ View/Modules/Web/home.php
@@ -84,10 +84,6 @@
             </nav>
         </div>        
 
-        <div id="home_requadres" style="text-align: center;">
-            <img src="http://www.ddgi.cat/ddgi/docNivell/200-anys/200_anys_casa_cultura.jpg" width="50%">
-        </div>
-
         <footer id="home_footer">            
             
             <div>
